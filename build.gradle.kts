59d072cfff635ed68402848a7a4ad14374a03e3a
CaioRS <caiorss.rodrigues@gmail.com>
2019-11-14 10:08:44 -0300

    Clean build.gradle.kts file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,6 @@ application {
 dependencies {
     compile(kotlin("stdlib"))
     compile("io.javalin:javalin:3.6.0")
-	// compile( "io.ktor:ktor-server-core:1.2.4")
-	//     compile("io.ktor:ktor-server-netty:1.2.4")
     compile( "org.slf4j:slf4j-simple:1.7.29")
 }
 
@@ -26,8 +24,7 @@ repositories {
 sourceSets {
     // Change source directory 
     getByName("main").java.srcDirs("src")
-    // getByName("androidTest").java.srcDirs("src/androidTest/kotlin")
-    // getByName("debug").java.srcDirs("src/debug/kotlin")    
+    // getByName("debug").java.srcDirs("src/debug/kotlin")
     // getByName("test").java.srcDirs("src/test/kotlin")
 }
 
@@ -51,12 +48,3 @@ tasks {
         dependsOn(fatJar)
     }
 }
-
-//sourceCompatibility = 1.8
-//
-//compileKotlin {
-//    kotlinOptions.jvmTarget = "1.8"
-//}
-//compileTestKotlin {
-//    kotlinOptions.jvmTarget = "1.8"
-//}
