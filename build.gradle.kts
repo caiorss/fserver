95823a28063b9fd1920573b4759f2cce241313ba
CaioRS <caiorss.rodrigues@gmail.com>
2019-11-20 13:05:03 -0300

    Add pdfbox library dependency in gradle build script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,10 @@ dependencies {
     compile("io.javalin:javalin:3.6.0")
     compile("org.slf4j:slf4j-simple:1.7.29")
     compile("com.github.ajalt:clikt:2.3.0")
-
     // See: https://github.com/mwanji/toml4j
     compile("com.moandjiezana.toml:toml4j:0.7.2")
+
+    compile("org.apache.pdfbox:pdfbox:2.0.17")
 
 }
 
